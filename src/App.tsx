52a3106cf4a6c18e07030debce2a6d36d1315b8d
Marius <mariuslangeland.ml@gmail.com>
2022-10-23 01:27:04 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactPropTypes } from 'react';
+
+interface IconType{type: string}
+function Icon(props : IconType){
+  return <span className={`material-symbols-outlined ${props.type}`}>{props.type}</span>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Navbar />
+        <Filtering />
+        <Result />
+    </>
+  );
+}
+
+function Navbar(){
+
+  function Logo(){
+    return(
+      <a className='link-home' href=''>
+        <img className='logo' alt='Logo' />
+      </a>
+    );
+  }
+  function Configs(){
+    return(
+      <span className='config-buttons'>
+        <span className='theme-switches'>
+          <Icon type='dark_mode'/>
+          <Icon type='light_mode'/>
+        </span>
+        <Icon type='language'/>
+        <Icon type='person'/>
+      </span>
+    );
+  }
+
+  return(
+      <section className="top-bar">
+        <Logo/>
+        <Configs/>
+      </section>
+  );
+}
+
+function Filtering(){
+
+  function Search(){
+    return(
+      <div className="search">
+        <Icon type='search'/>
+        <input type="text" name='book-name' className='book-name' placeholder='Search title, author, topic...'/>
+      </div>
+    );
+  }
+
+  function Categories(){
+    return(
+      <div className="categories">
+        <Icon type='filter_list'/>
+      </div>
+    );
+  }
+
+  return(
+    <section className="filtering">
+      <Search/>
+      <Categories/>
+    </section>
+  );
+}
+
+function Result(){
+  return(
+    <>
+    </>
   );
 }
 
 export default App;
+
