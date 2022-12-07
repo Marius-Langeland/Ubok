2453ba6ce08b872a38356e154cbf323a08a471bf
Marius-Langeland <mariuslangeland.ml@gmail.com>
2022-12-07 10:59:55 +0100

    Optimization

--- src/sections/Background.tsx
+++ src/sections/Background.tsx
@@ -13,13 +13,15 @@ function Background(){
   }, []);
 
   return(
-      <section className="background" style={{'filter': `blur(${Math.min(10, scroll / 70)}px)`, 'marginTop': `${-scroll / 3}px`}}>
-        <SiteLogo className='logo'/>
-        <div className="background-content">
-          <div>818 <br/> Bloggposter</div>
-          <div>1037 <br/> Forfattere</div>
-          <div>3287 <br/> Bøker</div>
-        </div>
+      <section className="background">
+        <span className="background-elements" style={{'filter': `blur(${Math.min(10, scroll / 70)}px)`, 'marginTop': `${Math.max(-(scroll * scroll)*0.001, -500)}px`}}>
+          <SiteLogo className='logo'/>
+          <div className="background-content">
+            <div>818 <br/> Bloggposter</div>
+            <div>1037 <br/> Forfattere</div>
+            <div>3287 <br/> Bøker</div>
+          </div>
+        </span>
       </section>
   );
 }
