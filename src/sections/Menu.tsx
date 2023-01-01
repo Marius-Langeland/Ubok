c2104ca9fb11a4893a90d5e0ce2ea527ce2161dc
Marius <mariuslangeland.ml@gmail.com>
2023-01-01 22:42:39 +0100

    Removed clutter

--- src/sections/Menu.tsx
+++ src/sections/Menu.tsx
@@ -6,23 +6,10 @@ function Menu(){
     return (
         <section className='menu-section shadow'>
             <span className="background"></span>
-            <Icon type="menu" className="menu-button"/>
-            <div className='settings'>
-                <Icon type='lightbulb'/>
-                <Icon type='empty_dashboard'/>
-                <Icon type='public'/>
-            </div>
             <span className="logo">Ubok</span>
-            <span className="profile shadow">
-                <span></span>
+            <span className="menu-button shadow">
+                <Icon type="menu"/>
             </span>
-            <div className="description">
-                <span>2543</span>
-                <span>42</span>
-                <span>672</span>
-                <span>15</span>
-                <span>781</span>
-            </div>
         </section>
     );
     
