2d9a946f7c14e67966c1bf1efba4d928339ce683
Marius <mariuslangeland.ml@gmail.com>
2023-03-09 22:02:28 +0100

    Working weekly-book

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,12 @@ import { WorksResponse } from './utils/forrigebokAPI';
 import API, { Type } from './utils/fetchForrigebok';
 
 function App() {
-  const [weekly, setWeekly] = useState({});
+  const [weekly, setWeekly] = useState<WorksResponse | undefined>();
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   
-
   useEffect(() => {
-    setWeekly({});
+    setWeekly(undefined);
     setError(false);
     setLoading(true);
 
@@ -24,18 +23,19 @@ function App() {
       .then(setWeekly)
       .catch(() => setError(true))
       .finally(() => setLoading(false));
-  }, []);
+   }, []);
 
   return (
     <>
       <Menu />
       <span className='gap'></span>
       <section id="motd">
-        <BookCover id="weekly-book"/>
+        <BookCover coverOnly={true} id="weekly-book" work={weekly?.works[0]}/>
         <h3>Ukens bok</h3>
         <span id="timer">6d 3t 24m</span>
-        <span id="title">Title auf gaufmenchinnen</span>
-        <span id="author">av Author von Bismarch</span>
+        <span id="description">{weekly?.works[0].simplifiedPresentationMetadata.description}</span>
+        <span id="title">{weekly?.works[0].simplifiedPresentationMetadata.title ?? "Ukens bok"}</span>
+        <span id="author">{`av ${weekly?.works[0].simplifiedPresentationMetadata.authors[0] ?? "ukjent forfatter"}`}</span>
       </section>
       
       <section id="weekly-vote">
--- src/components/BookCover.tsx
+++ src/components/BookCover.tsx
@@ -2,20 +2,16 @@ import React, {useState} from 'react'
 import './bookCover.scss'
 
 function BookCover(props: any){
+    const work = props.work;
 
-    if(props.template == true || !props.books)
+    if(props.template || !work)
         return <div className='book-cover book-cover-template' id={props.id}>
             <img src="https://cdn.discordapp.com/attachments/1058068137783865465/1059590610434134046/Geezmo_abstract_illustration_fb20c988-0999-4e4f-b881-3d0913ceb89a.png" alt="" />
         </div>
 
     return(
-        <div className="book-cover" id={props.id}>
-            <img src={props.book.cover} alt="" />
-            <div className="book-info">
-                <span className="book-author">{props.book.author}</span>
-                <span className="book-description">{props.book.description}</span>
-                <span className="target-audience">{props.book.targetAudience}</span>
-            </div>
+        <div className="book-cover">
+            <img className='book-cover' src={work.cover} alt={`The cover of "${work.title}"`} />
         </div>
     );
 }
